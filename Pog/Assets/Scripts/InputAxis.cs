9ab1d1b44ca6176f03afc10806332d8a1cbe27e4
selin <68879061+shurriyetmq@users.noreply.github.com>
2021-10-06 14:51:10 +1100

    Finished main scene v2

--- Pog/Assets/Scripts/InputAxis.cs
+++ Pog/Assets/Scripts/InputAxis.cs
@@ -6,4 +6,6 @@ public class InputAxis
 {
     public const string Vertical = "Vertical";
     public const string Horizontal = "Horizontal";
+    public const string verticalPlayer2 = "Vertical Player 2";
+    public const string horizontalPlayer2 = "Horizontal Player 2";
 }
--- Pog/Assets/Scripts/Move.cs
+++ Pog/Assets/Scripts/Move.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public float speed = 3;
+    private int player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (gameObject.CompareTag("Player1"))
+        {
+            player = 1;
+        }
+
+        if (gameObject.CompareTag("Player2"))
+        {
+            player = 2;
+        }
     }
 
     // Update is called once per frame
@@ -17,22 +26,46 @@ public class Move : MonoBehaviour
     {
         float Horizontal = Input.GetAxis(InputAxis.Horizontal);
         float Vertical = Input.GetAxis(InputAxis.Vertical);
-        
-        if (Vertical > 0)
+        float horizontalPlayer2 = Input.GetAxis(InputAxis.horizontalPlayer2);
+        float verticalPlayer2 = Input.GetAxis(InputAxis.verticalPlayer2);
+
+        if (player == 1)
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        }
-        if (Vertical < 0)
+          if (Vertical > 0)
+          {
+              transform.Translate(Vector3.forward * speed * Time.deltaTime);
+          }
+          if (Vertical < 0)
+          {
+              transform.Translate(Vector3.back * speed * Time.deltaTime);
+          }
+          if (Horizontal > 0)
+          {
+              transform.Translate(Vector3.right * speed * Time.deltaTime);
+          }
+          if (Horizontal < 0)
+          {
+             transform.Translate(Vector3.left * speed * Time.deltaTime);
+          }
+        } 
+        else
         {
-            transform.Translate(Vector3.back * speed * Time.deltaTime);
-        }
-        if (Horizontal > 0)
-        {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }
-        if (Horizontal < 0)
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
+          if (verticalPlayer2 > 0)
+          {
+              transform.Translate(Vector3.forward * speed * Time.deltaTime);
+          }
+          if (verticalPlayer2 < 0)
+          {
+              transform.Translate(Vector3.back * speed * Time.deltaTime);
+          }
+          if (horizontalPlayer2 > 0)
+          {
+              transform.Translate(Vector3.right * speed * Time.deltaTime);
+          }
+          if (horizontalPlayer2 < 0)
+          {
+             transform.Translate(Vector3.left * speed * Time.deltaTime);
+          }
+        } 
     }
 }
